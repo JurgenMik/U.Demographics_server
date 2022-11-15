de083a08047ddedfadd2de79878eb391dd405428
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-11-16 00:06:28 +0300

    Changed server :port

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const port = 3000;
+const port = 3001;
 
 const userRouter = require("./routes/users");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/user-info", userRouter);
-
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
