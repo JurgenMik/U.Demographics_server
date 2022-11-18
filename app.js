00a3a9c2578bfa6b747a209d3224c17304a13b39
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-11-19 01:57:43 +0300

    Enabled https with OpenSSL

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const http = require('https');
+const fs = require('fs');
 const app = express();
+const path = require('path');
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = 3001;
@@ -10,6 +13,15 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/user-info", userRouter);
 
+/*
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
 })
+ */
+
+const sslServer = http.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+}, app)
+
+sslServer.listen(port, () => console.log(`Server started on port: ${port}`));
